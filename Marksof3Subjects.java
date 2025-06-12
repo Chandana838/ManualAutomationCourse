d9fd03b04d4430b92daa2975a228576794a55caf
Chandana838 <chandanareddy446@gmail.com>
2025-06-12 16:14:51 +0530

    Add files via upload

---
+++ Marksof3Subjects.java
@@ -0,0 +1,16 @@
+package firstpackage;
+
+public class Marksof3Subjects {
+
+	public static void main(String[] args) {
+		int maths=50;
+		int science=30;
+		int english=20;
+		int Totalmarks=(maths+science+english);
+		int Averagemarks=((maths+science+english)/3);
+		System.out.println("Total marks="+Totalmarks);
+		System.out.println("Average marks="+Averagemarks);
+		
+	}
+
+}
