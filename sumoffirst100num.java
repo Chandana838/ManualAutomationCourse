be5dce5e957084aedf799de7c92b31a5cc80c0da
Chandana838 <chandanareddy446@gmail.com>
2025-06-12 18:21:36 +0530

    Add files via upload

---
+++ sumoffirst100num.java
@@ -0,0 +1,11 @@
+package conditionalstatement;
+
+public class sumoffirst100num {
+
+	public static void main(String[] args) {
+		int i,sum=0;
+		for(i=1;i<=100;i++){
+sum=sum+i;
+	}
+System.out.println(sum);
+}}
