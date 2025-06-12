ed2e389c29b87c73aa33516bc5334474d78a643f
Chandana838 <chandanareddy446@gmail.com>
2025-06-12 18:55:41 +0530

    Add files via upload

---
+++ Factorialofanumber.java
@@ -0,0 +1,12 @@
+package conditionalstatement;
+public class Factorialofanumber {
+	public static void main(String[] args) {
+		int i,fac=6;
+		int j=1;
+		for(i=1;i<=fac;i++)
+		{
+		j=i*j;	
+		}
+		System.out.println(j);
+	}
+}
